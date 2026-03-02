4244f67ebfc6d327b7277cd4b2b96065add9b6dc
H-Drozdowski <hdrozdowski@rrc.ca>
2026-03-02 17:17:46 -0600

    feat: Added a regular expression to validate the email input value.

--- script.js
+++ script.js
@@ -33,15 +33,20 @@ const validateForm = () => {
     // Clear error messages
     clearErrorMessages()
 
+    // Regular expression for email validation 
+    const complexEmailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
     // Elements to check validation
     const name = document.getElementById("name")
     const favCharactersList = document.getElementsByName("favCharacter")
     const favEpisode = document.getElementById("favoriteEpisode")
+    const email = document.getElementById("email")
 
     // For when I want the error to display on a specific location
     const radioErrorContainer = document.getElementById("radioErrorContainer")
     const nameContainer = document.getElementById("nameInputContainer")
     const favEpisodeContainer = document.getElementById("favoriteEpisodeContainer")
+    const emailContainer = document.getElementById("emailContainer")
 
     // Name validation
     if(name.value === ""){
@@ -63,6 +68,11 @@ const validateForm = () => {
     else if (favEpisode.value <= 0 || favEpisode.value > 148){
         isValid = false
         addInputError(favEpisodeContainer, "Favorite episode must be between in the range of existing episodes (1 - 148)")
+    }
+
+    if (!complexEmailPattern.test(email.value)){
+        addInputError(emailContainer, "Please enter a valid email.")
+        isValid = false
     }
 
     return isValid
